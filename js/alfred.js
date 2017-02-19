a7fefb9509e269c7b9628ded0702f66d61f06ff2
Перевощиков <gangstatracer@gmail.com>
2017-02-19 14:26:12 +0500

    ported to knockout.js

--- js/alfred.js
+++ js/alfred.js
@@ -3,9 +3,6 @@ const field_height = 6;
 const field_width = 12;
 const number_of_walls = 15;
 
-var field = [];
-var robot_start = {};
-
 var AlfredModel = function() {
 	var self = this;
 	
@@ -25,145 +22,56 @@ var AlfredModel = function() {
 	}
 	
 	//field section
-	self.field = ko.observableArray();
-	for(var i = 0; i < field_height; i++) {
-		var row = ko.observableArray();
-		for(var j = 0; j < field[i].length; j++) {
-			row.push(ELEMENTS['space']);
-		}
-		self.field.push(row);
-	}
-}
-ko.applyBindings(new AlfredModel());
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  while(true) {
-    if ((new Date().getTime() - start) > milliseconds){
-      return;
-    }
-  }
-}
-
-function put_to_empty_space( field, src ) {
-	var put = false;
-	while(!put) {
-		var x = Math.floor(Math.random() * field_height);
-		var y = Math.floor(Math.random() * field_width);
-		if(field[x][y] == ELEMENTS['space']) {
-			field[x][y] = src;
-			put = true;
+	
+	self.randomEmptyPoint = function () {
+		while(true) {
+			var x = Math.floor(Math.random() * field_height);
+			var y = Math.floor(Math.random() * field_width);
+			if(self.field()[x]()[y].src() == ELEMENTS['space']) {
+				return {x: x, y: y};
+			}	
 		}	
 	}
-}
 
-function init_field () {
-	var field = new Array(field_height);
-	for(var i = 0; i < field.length; i++) {
-		field[i] = new Array(field_width);
-		for(var j = 0; j < field[i].length; j++) {
-			field[i][j] = ELEMENTS['space'];
+	self.putToPoint = function (point, value) {
+		self.field()[point.x]()[point.y].src(value);
+	}
+	
+	self.new_game = function () {
+		for(var i = 0; i < self.field().length; i++ ){
+			for(var j = 0; j < self.field()[i]().length; j++ ){
+				self.putToPoint({x: i, y: j}, ELEMENTS['space']);
+			}
+		}
+		self.robot = self.randomEmptyPoint();
+		self.putToPoint(self.robot, ELEMENTS['robot']);
+
+		self.battery = self.randomEmptyPoint();	
+		self.putToPoint(self.battery, ELEMENTS['battery']);
+
+		for(var i = 0; i < number_of_walls; i++){
+			self.putToPoint(self.randomEmptyPoint(), ELEMENTS['wall']);
 		}
 	}
-
-	put_to_empty_space(field, ELEMENTS['robot']);
-	put_to_empty_space(field, ELEMENTS['battery'])
-
-	for(var i = 0; i < number_of_walls; i++) {
-		put_to_empty_space(field, ELEMENTS['wall']);
+	
+	self.GameFieldCell = function (src) {
+		this.src = ko.observable(src);
 	}
-	return field;
-}
-
-function render_field (field) {
-	var grid = $('#grid'), row = 0, column = 0;
-	grid.children().each(function () {
-		column = 0;
-		$(this).children().each(function () {
-			$(this).children().attr('src', field[row][column]);
-			column += 1;
-		});
-		row += 1;
-	});	
-}
-
-function init () {
-	field = init_field();
-	render_field(field)
-}
-
-function render_item (item, point) {
-	field[point.x][point.y] = item;
-	render_field(field);
+	
+	self.field = ko.observableArray();
+	
+	for(var i = 0; i < field_height; i++) {
+		self.field.push(ko.observableArray());
+		for(var j = 0; j < field_width; j++) {
+			self.field()[i]().push(new self.GameFieldCell(ELEMENTS['space']));
+		}
+	}
+	
+	self.new_game();
 }
 
 $(document).ready(function () {
 	
-	init();	
-    
-	$('.js-new-game').on('click', function (e) {
-        e.preventDefault();
-		init();       
-    });
-	
-	$('.js-reset').on('click', function (e) {
-		e.preventDefault();
-		render_field(field);
-	});
-	
-	$('.js-run').on('click', function (e) {
-		var robot = { "x": 0, "y": 0};
-		for(var i = 0; i < field.length; i++) {
-			for(var j = 0; j < field[i].length; j++) {
-				if(field[i][j] == ELEMENTS['robot']) {
-					robot.x = i;
-					robot.y = j;
-				}
-			}
-		}
-		robot_start = jQuery.extend({}, robot);
-		
-		$('#commands-list').children().each(function (index) {
-			var row = $(this), vector = {"x": 0, "y": 0};
-			var command = row.children('.command').text();
-			row.addClass('info');
-				switch(command) {
-					case 'Вверх':
-						vector.x--;
-						break;
-					case 'Вниз':
-						vector.x++;
-						break;
-					case 'Вправо':
-						vector.y++;
-						break;
-					case 'Влево':
-						vector.y--;
-						break;
-				}
-			render_item(ELEMENTS['space'], robot);
-			robot = {"x": robot.x + vector.x, "y": robot.y + vector.y}
-			render_item(ELEMENTS['robot'], robot);
-			sleep(500);
-			row.removeClass('info');			
-		});
-	});
-	
-	$('.js-add-command').on('click', function (e) {
-		e.preventDefault();
-		$('#commands-list').append(
-			'<tr><td class="command-number">' 
-			+ ($('#commands-list').children().length + 1) 
-			+ '</td><td class="command">' 
-			+ $('#command-selection option:selected').text() 
-			+ '</td><td><span class="glyphicon glyphicon-remove js-remove-command" aria-hidden="true"></span></td></tr>');
-	});
-	
-	$('#commands-list').on('click', '.js-remove-command', function (e) {
-		$(this).parent().parent().remove();
-		$('#commands-list').children().each(function (index) {
-			$(this).children('.command-number').text(index + 1);
-		});
-	});
+	ko.applyBindings(new AlfredModel());
 	
 });
